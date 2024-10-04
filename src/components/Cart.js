79a624c659350fa94ebe8b1a0daa75ae9318f3d6
sakship13 <141441050+sakship13@users.noreply.github.com>
2024-10-04 04:24:28 -0700

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect,useState } from 'react'
+import React, { useContext } from 'react'
 import {StoreContext} from './StoreContext'
 import { Link, useNavigate } from 'react-router-dom';
 function Cart() {
